cb4b5533efdd3cbfa91ebb76d53919c1e3518501
krylowicz <kacper@krylowicz.pl>
2022-06-08 20:24:47 +0200

    to_one_hot fix

--- tinynet/utils.py
+++ tinynet/utils.py
@@ -1,10 +1,10 @@
 import numpy as np
-from .tensor import Tensor
+from tinynet.tensor import Tensor
 
 
 def to_one_hot(labels: Tensor, n_classes: int) -> Tensor:
     labels = labels.data.astype(int)
     one_hot = np.zeros((labels.shape[0], n_classes))
-    one_hot[np.arange(labels.shape[0]), labels] = 1
+    one_hot[np.arange(one_hot.shape[0]), labels] = 1
 
     return Tensor(one_hot, requires_grad=True)
