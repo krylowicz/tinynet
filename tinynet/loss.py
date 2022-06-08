c56f3789e7566c9856e28350265ed7ac9bfc6b9d
krylowicz <kacper@krylowicz.pl>
2022-06-08 20:37:09 +0200

    Cross Entropy Loss class

--- tinynet/loss.py
+++ tinynet/loss.py
@@ -1,4 +1,5 @@
 from tinynet.tensor import Tensor
+from tinynet.utils import to_one_hot
 
 
 class Loss:
@@ -10,3 +11,22 @@ class Loss:
 
     def forward(self, *args):
         raise NotImplementedError("You must implement forward function for custom loss")
+
+
+class CrossEntropyLoss(Loss):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, predicted: Tensor, target: Tensor) -> Tensor:
+        """
+        Computes the cross entropy loss between input and target
+
+        predicted: (batch_size, n_classes) raw, un-normalized scores for each class
+        target: range [0, n_classes)
+        """
+        batch_size, n_classes = predicted.shape
+
+        predicted = predicted.logsoftmax()
+        target = to_one_hot(target, n_classes)
+
+        return (-target * predicted).mean()
