9753fa8889589c176a6ea5af060ef252c9c78665
krylowicz <kacper@krylowicz.pl>
2022-07-23 09:44:04 +0200

    binary_op() helper function

--- tinynet/ops_gpu.py
+++ tinynet/ops_gpu.py
@@ -16,21 +16,25 @@ def determine_shape(x_shape: tuple[int, ...], y_shape: tuple[int, ...]) -> tuple
     return np.broadcast_shapes(x_shape, y_shape)
 
 
+def binary_op(ctx: Context, name: str, op_fn: str, x: Tensor, y: Tensor) -> Tensor:
+    ret = new_cl_buffer(ctx.cl_ctx, determine_shape(x.shape, y.shape))
+    prg = cl.Program(ctx.cl_ctx, f"""
+        __kernel void {name}(__global const float *x, __global const float *y, __global float *ret) {{
+            int g_id = get_global_id(0);
+            ret[g_id] = x[gid] {op_fn} y[gid]; 
+        }}
+    """).build()
+    prg.__dict__[name](ctx.cl_queue, (ret.size // 4,), None, x.data, y.data, ret)
+    requires_grad = x.requires_grad or y.requires_grad
+
+    return Tensor(ret, requires_grad=requires_grad)
+
+
 @Function.register(gpu=True)
 class Add(Function):
     @staticmethod
     def forward(ctx: Context, x: Tensor, y: Tensor) -> Tensor:
-        ret = new_cl_buffer(ctx.cl_ctx, determine_shape(x.shape, y.shape))
-        prg = cl.Program(ctx.cl_ctx, """
-            __kernel void add(__global const float *x, __global const float *y, __global float *ret) {
-                int g_id = get_global_id(0);
-                ret[g_id] = x[g_id] + y[g_id];
-            }
-        """).build()
-        prg.add(ctx.cl_queue, (ret.size//4,), None, x.data, y.data, ret)
-        requires_grad = x.requires_grad or y.requires_grad
-
-        return Tensor(ret, requires_grad=requires_grad)
+        return binary_op(ctx, "add", "+", x, y)
 
     @staticmethod
     def backward(ctx: Context, grad_output: Tensor) -> tuple[Tensor, Tensor]:
@@ -41,17 +45,7 @@ class Add(Function):
 class Sub(Function):
     @staticmethod
     def forward(ctx: Context, x: Tensor, y: Tensor) -> Tensor:
-        ret = new_cl_buffer(ctx.cl_ctx, determine_shape(x.shape, y.shape))
-        prg = cl.Program(ctx.cl_ctx, """
-            __kernel void sub(__global const float *x, __global const float *y, __global float *ret) {
-                int g_id = get_global_id(0);
-                ret[g_id] = x[g_id] - y[g_id];
-            }
-        """).build()
-        prg.sub(ctx.cl_queue, (ret.size//4,), None, x.data, y.data, ret)
-        requires_grad = x.requires_grad or y.requires_grad
-
-        return Tensor(ret, requires_grad=requires_grad)
+        return binary_op(ctx, "sub", "-", x, y)
 
     @staticmethod
     def backward(ctx: Context, grad_output: Tensor) -> tuple[Tensor, Tensor]:
@@ -62,17 +56,7 @@ class Sub(Function):
 class Mul(Function):
     @staticmethod
     def forward(ctx: Context, x: Tensor, y: Tensor) -> Tensor:
-        ret = new_cl_buffer(ctx.cl_ctx, determine_shape(x.shape, y.shape))
-        prg = cl.Program(ctx.cl_ctx, """
-            __kernel void mul(__global const float *x, __global const float *y, __global float *ret) {
-                int g_id = get_global_id(0);
-                ret[g_id] = x[g_id] * y[g_id];
-            }
-        """).build()
-        prg.mul(ctx.cl_queue, (ret.size//4,), None, x.data, y.data, ret)
-        requires_grad = x.requires_grad or y.requires_grad
-
-        return Tensor(ret, requires_grad=requires_grad)
+        return binary_op(ctx, "mul", "*", x, y)
 
     @staticmethod
     def backward(ctx: Context, grad_output: Tensor) -> tuple[Tensor, Tensor]:
